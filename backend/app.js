d667cb3462331c051997eb7b27208d3bdcba501e
camfung <cameronfuung@gmail.com>
2024-10-09 14:02:26 -0700

    added mock end points

--- backend/app.js
+++ backend/app.js
@@ -9,13 +9,37 @@ class App {
         app.use(this.incrementRequestCount.bind(this));
         app.cors(["http://127.0.0.1:8080"]);
 
-        app.post('/api/sql', (req, res) => {
-            res.send("<p>post req recieved</p>")
+        app.post('/api/insertrecords', (req, res) => {
+            res.json([{
+                parentId: 1,
+                name: "Sara Brown",
+                dateOfBirth: "1901-01-01",
+            },
+            {
+                parentId: 1,
+                name: "John Smith",
+                dateOfBirth: "1941-01-01",
+            },
+            {
+                parentId: 1,
+                name: "Jack Ma",
+                dateOfBirth: "1961-01-30",
+            },
+            {
+                parentId: 1,
+                name: "Elon Musk",
+                dateOfBirth: "1999-01-01",
+            }
 
+            ])
         });
 
-        app.get('/api/sql', (req, res) => {
+        app.get('/api/query', (req, res) => {
             res.send("<p>get req recieved</p>")
+        });
+
+        app.post('/api/query', (req, res) => {
+            res.send("<p>query post req recieved</p>")
         });
 
         app.listen(app.DEFAULT_PORT, () => {
